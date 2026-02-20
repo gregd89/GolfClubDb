f68551600b5b15ed913e122ca781f302362997d5
Greg Dunne <gregd89@users.noreply.github.com>
2026-02-20 10:58:24 +0000

    added datepicker and time slots

--- GolfClubDb/Pages/Bookings/Create.cshtml.cs
+++ GolfClubDb/Pages/Bookings/Create.cshtml.cs
@@ -8,19 +8,22 @@ namespace GolfClubDb.Pages.Bookings
     {
         private readonly GolfClubDb.Data.ApplicationDbContext _context;
 
+        // Constructor to inject the ApplicationDbContext
         public CreateModel(GolfClubDb.Data.ApplicationDbContext context)
         {
             _context = context;
         }
 
+
+        // GET handler to display the create page
         public IActionResult OnGet()
         {
             ViewData["MemberId"] = new SelectList(_context.Member, "Id", "Id");
+            TimeOptions = GenerateTimeOptions();
             return Page();
         }
 
-        [BindProperty]
-        public GolfClubDb.Models.Bookings Bookings { get; set; } = default!;
+
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
@@ -35,5 +38,36 @@ namespace GolfClubDb.Pages.Bookings
 
             return RedirectToPage("./Index");
         }
+
+        // BindProperty to bind the form data for the booking date
+        [BindProperty]
+        public DateOnly? BookingDate { get; set; }
+
+        // BindProperty to bind the form data to the Bookings model
+        [BindProperty]
+        public GolfClubDb.Models.Bookings Bookings { get; set; } = default!;
+
+
+        // List of time options for the booking
+        public List<SelectListItem> TimeOptions { get; set; } = new();
+
+        // Method to generate time options in 15-minute intervals from 9:00 to 18:00
+        private List<SelectListItem> GenerateTimeOptions()
+        {
+            var items = new List<SelectListItem>();
+            var start = new TimeOnly(9, 0);
+            var end = new TimeOnly(18, 0);
+
+            for (var s = start; s <= end; s = s.AddMinutes(15))
+            {
+                var label = s.ToString("HH:mm");
+                items.Add(new SelectListItem { Value = label, Text = label });
+
+            }
+
+            return items;
+        }
+
+
     }
 }
